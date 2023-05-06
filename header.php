87ce3ac80cd70488ba1aff11878dff5e74d72043
Balazs527 <strauszbalazs2001@gmail.com>
2023-05-06 19:20:23 +0200

    ..................................................

--- header.php
+++ header.php
@@ -6,12 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Login & Registration</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="dark-theme.css" rel="stylesheet">
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="index.php">My Website</a>
+            <a class="navbar-brand" href="index.php">School Stuff</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -26,6 +27,7 @@
                         echo '<li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>';
                     } else {
                         echo '<li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>';
+                        echo '<li class="nav-item"><a class="nav-link" href="register.php">Registration</a></li>';
                     }
                     ?>
                 </ul>
