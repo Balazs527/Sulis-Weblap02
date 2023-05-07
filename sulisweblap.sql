c43f9037a1834fee41456580d42a50cdcf125193
Balazs527 <strauszbalazs2001@gmail.com>
2023-05-07 18:51:44 +0200

    cars tábla + leírása hozzáadása az adatbázishoz...

--- sulisweblap.sql
+++ sulisweblap.sql
@@ -2,7 +2,7 @@ CREATE DATABASE sulisweblap;
 
 USE sulisweblap;
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     firstname VARCHAR(255) NOT NULL,
     lastname VARCHAR(255) NOT NULL,
@@ -15,3 +15,25 @@ CREATE TABLE IF NOT EXISTS users (
 INSERT INTO users (firstname, lastname, username, email, password, role)
 VALUES ('user', '1', 'user1', 'user1@example.com', 'password1', 'user'),
        ('admin', '1', 'admin1', 'admin1@example.com', 'adminpassword1', 'admin');
+
+CREATE TABLE cars (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    users_username VARCHAR(255) NOT NULL,
+    made_by VARCHAR(255) NOT NULL,
+    model VARCHAR(255) NOT NULL,
+    year INT NOT NULL,
+    color VARCHAR(255) NOT NULL,
+    engine_size FLOAT NOT NULL,
+    fuel_type ENUM('gasoline', 'diesel', 'electric', 'hybrid') NOT NULL,
+    description TEXT,
+    FOREIGN KEY (users_username) REFERENCES users(username)
+);
+/*id: az autó egyedi azonosítója
+users_username: a tulajdonos felhasználóneve, ami a users táblához kapcsolódik
+made_by: az autó gyártója
+model: az autó modellje
+year: az autó gyártási éve
+color: az autó színe
+engine_size: a motor mérete literben
+fuel_type: az üzemanyag típusa (benzin, dízel, elektromos vagy hibrid)
+description: egy leírás az autóról*/
