881a97c6c3c25ef95df9a50b97933515e7bdc13c
Balazs527 <strauszbalazs2001@gmail.com>
2023-05-07 16:50:02 +0200

    új navbar.........................................

--- header.php
+++ header.php
@@ -11,17 +11,17 @@
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand nav-link" href="index.php">School Stuff</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php">Home</a>
-                    </li>
-                    <?php
+    <div class="container-fluid">
+        <a class="navbar-brand nav-link" href="index.php">School Stuff</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <?php
                     session_start();
                     if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']) {
                         echo '<li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>';
@@ -29,8 +29,23 @@
                         echo '<li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>';
                         echo '<li class="nav-item"><a class="nav-link" href="register.php">Registration</a></li>';
                     }
-                    ?>
-                </ul>
-            </div>
+                ?>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <form class="d-flex ms-auto" role="search">
+                <input class="form-control me-2 dark-input" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
         </div>
-    </nav>
+    </div>
+</nav>
