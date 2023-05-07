d8b80da8ad22896b5b82b947c8cc7f609e823595
Balazs527 <strauszbalazs2001@gmail.com>
2023-05-07 17:02:05 +0200

    navbar és css módosítások.........................

--- header.php
+++ header.php
@@ -34,11 +34,11 @@
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Dropdown
                     </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    <ul class="dropdown-menu dark-input">
+                        <li><a class="dropdown-item dark-input" href="#">Action</a></li>
+                        <li><a class="dropdown-item dark-input" href="#">Another action</a></li>
+                        <li><hr class="dropdown-divider dark-input"></li>
+                        <li><a class="dropdown-item dark-input" href="#">Something else here</a></li>
                     </ul>
                 </li>
             </ul>
